3a50ac6b81c525954a8fba08a42553ccb6bc7dd9
hsumyatmon1 <hsumyatmon1@my.jcu.edu.au>
2023-09-06 00:13:22 +0800

    Test deploy the app

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ import { Interactable } from "../../libs/Interactable.js";
 import { Player } from "../../libs/Player.js";
 import { LoadingBar } from "../../libs/LoadingBar.js";
 
+// Deploying the App
+
+const express = require("express");
+const app = express();
+
+// This line is important to ensure your app listens to the PORT env var
+const port = process.env.PORT ?? 8080;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
 // Define a JavaScript class named "App".
 class App {
     // Constructor function for the "App" class.

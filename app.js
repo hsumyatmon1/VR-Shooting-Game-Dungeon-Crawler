45be8c4947efae8018d1e0b9be94d1963867753e
hsumyatmon1 <hsumyatmon1@my.jcu.edu.au>
2023-09-05 23:50:48 +0800

    Load 3D environment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import { XRControllerModelFactory } from "../../libs/three/jsm/XRControllerModel
 import { Pathfinding } from "../../libs/three/jsm/three-pathfinding.module.js";
 import { Stats } from "../../libs/stats.module.js";
 import { VRButton } from "../../libs/VRButton.js";
+import { TeleportMesh } from "../../libs/TeleportMesh.js";
 import { Interactable } from "../../libs/Interactable.js";
 import { Player } from "../../libs/Player.js";
 import { LoadingBar } from "../../libs/LoadingBar.js";
@@ -93,4 +94,119 @@ class App {
         // the Three.js library.The code imports various modules, creates a 3D scene,
         // sets up lighting, and prepares for rendering in a web - based VR environment.
     }
+
+    // A function to resize the renderer when the window size changes.
+    resize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    // A function to set the 3D environment's background using an HDR image.
+    setEnvironment() {
+        const loader = new RGBELoader().setDataType(THREE.UnsignedByteType);
+        const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+        pmremGenerator.compileEquirectangularShader();
+
+        const self = this;
+
+        // Load the HDR environment map and set it as the scene's environment.
+        loader.load(
+            "../../assets/hdr/venice_sunset_1k.hdr",
+            (texture) => {
+                const envMap =
+                    pmremGenerator.fromEquirectangular(texture).texture;
+                pmremGenerator.dispose();
+
+                self.scene.environment = envMap;
+            },
+            undefined,
+            (err) => {
+                console.error("An error occurred setting the environment");
+            }
+        );
+    }
+
+    // Load the 3D environment (e.g., the dungeon) from a GLTF model.
+    loadEnvironment() {
+        const loader = new GLTFLoader().setPath(this.assetsPath);
+        const self = this;
+
+        this.interactables = [];
+
+        // Load the GLTF resource representing the environment.
+        loader.load(
+            "dungeon.glb",
+            function (gltf) {
+                self.scene.add(gltf.scene);
+
+                // Traverse the loaded 3D scene and configure its components.
+                gltf.scene.traverse(function (child) {
+                    if (child.isMesh) {
+                        if (child.name == "Navmesh") {
+                            // Hide the Navmesh by making it invisible.
+                            child.material.visible = false;
+                            self.navmesh = child;
+                            // Scale and position the Navmesh.
+                            child.geometry.scale(0.5, 0.5, 0.5);
+                            self.navmesh.scale.set(2, 2, 2);
+                        } else {
+                            if (child.name == "SD_Prop_Chest_Skull_Lid_01") {
+                                // Create an Interactable object for a chest lid.
+                                self.interactables.push(
+                                    new Interactable(child, {
+                                        mode: "tweens",
+                                        tweens: [
+                                            {
+                                                target: child.quaternion,
+                                                channel: "x",
+                                                start: 0,
+                                                end: -0.7,
+                                                duration: 1,
+                                            },
+                                        ],
+                                    })
+                                );
+                            } else if (child.name == "Door_1") {
+                                // Create an Interactable object for a door.
+                                self.interactables.push(
+                                    new Interactable(child, {
+                                        mode: "tweens",
+                                        tweens: [
+                                            {
+                                                target: child.quaternion,
+                                                channel: "z",
+                                                start: 0,
+                                                end: 0.6,
+                                                duration: 1,
+                                            },
+                                        ],
+                                    })
+                                );
+                            }
+                            child.castShadow = false;
+                            child.receiveShadow = true;
+                        }
+                    }
+                });
+
+                const scale = 0.5;
+                gltf.scene.scale.set(scale, scale, scale);
+
+                // Initialize pathfinding and load the Ghoul character.
+                self.initPathfinding();
+                self.loadGhoul();
+            },
+            function (xhr) {
+                self.loadingBar.progress = (xhr.loaded / xhr.total) * 0.5;
+            },
+            function (error) {
+                console.error(error.message);
+            }
+        );
+    }
+
+    // In this part of the code, we handle resizing the viewport,
+    // setting up the 3D environment's background, and loading the dungeon environment.
+    // We also create interactable objects like chest lids and doors within the environment.
 }
